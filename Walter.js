71d2bf8add5bdf050342a631f929ca8f5fdac657
Lucas <lucas@agtlucas.com>
2016-01-21 10:27:11 -0200

    Updates

---
+++ Walter.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const WALTER_PRICE = 0.53
+const TARE_PRICE = 0.08
+
+export default class Walter {
+
+  // input: initialMoney
+  // output: [totalWalters, moneyLeft]
+
+  round (f) {
+    return Math.round(f * Math.pow(10, 2)) / Math.pow(10, 2)
+  }
+
+  theCalculator (initialMoney) {
+    if (initialMoney <= 0) {
+      return false
+    }
+
+    let someWalters = 0
+
+    let bottles = this.numberOfBottles(initialMoney)
+    let price = this.thePrice(bottles)
+    let theMoneyIStillhave = initialMoney - price
+    let theTotal = theMoneyIStillhave + bottles * TARE_PRICE
+    let theIreration = this.numberOfBottles(theTotal)
+
+    if (theIreration) {
+      [someWalters, theTotal] = this.theCalculator(theTotal)
+    }
+
+    return [someWalters + bottles, this.round(theTotal)]
+
+  }
+
+  thePrice (theBottles) {
+    return theBottles * WALTER_PRICE
+  }
+
+  numberOfBottles (theAmount) {
+    return Math.floor(theAmount / WALTER_PRICE)
+  }
+
+}
---
+++ index.js
@@ -0,0 +1,5 @@
+import Walter from './Walter'
+
+let walter = new Walter()
+
+console.log(walter.theCalculator(3))
